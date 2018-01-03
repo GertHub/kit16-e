ebabf29b3ae377baa8b427d55117e63ebccb51b9
Tehnikumi Opilane <opilane@tptlive.ee>
2018-01-03 13:01:03 +0200

    Esimene commit, 03.01.2018, index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+$tervitus = 'hello';
+$nr = 1;
+$nr = 10.0121;
+$bool = False;
+$massiiv = array('Nimi', 1974,10.81, 'Perenimi');
+
+echo "$tervitus world! <br>";
+echo '$tervitus world! <br>';
+echo $massiiv[0];
+?>
